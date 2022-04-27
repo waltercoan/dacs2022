efa5e6b0979c9d5b6261b66209447090546fa8ac
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-04-27 20:25:34 -0300

    first commit

---
+++ src/main/java/br/univille/dacs2022/controller/HomeController.java
@@ -0,0 +1,15 @@
+package br.univille.dacs2022.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    @ResponseBody
+    public String index(){
+        return "Eu nao acredito que isso funciona...";
+    }
+}
