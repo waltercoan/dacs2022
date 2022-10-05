d8069ebd657343b834e59e77e7867e475d72c84a
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-10-05 14:21:53 -0300

    configurcacao correta jaeger

--- apidacs2022/src/main/java/br/univille/apidacs2022/opentrace/JaegerConfig.java
+++ apidacs2022/src/main/java/br/univille/apidacs2022/opentrace/JaegerConfig.java
@@ -1,24 +1,31 @@
 package br.univille.apidacs2022.opentrace;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.jaegertracing.Configuration.ReporterConfiguration;
 import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.Configuration.SenderConfiguration;
 import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.samplers.ConstSampler;
 import io.jaegertracing.internal.samplers.RateLimitingSampler;
 
 @Configuration
 public class JaegerConfig {
+    @Value("${dacs2022.jaeger.server}")
+    private String JAEGER_SERVER;
     @Bean
     public JaegerTracer jaegerTracer(){
+        var sampler = new SamplerConfiguration().withType(ConstSampler.TYPE)
+        .withParam(1);
+        var reporter = new ReporterConfiguration().withLogSpans(true)
+        .withSender(new SenderConfiguration().withAgentHost(JAEGER_SERVER));
         return new io.jaegertracing.Configuration("dacsapi-waltercoan")
-            .withSampler(new SamplerConfiguration().withType(ConstSampler.TYPE)
-            .withParam(1)) //10 Traces por segundo
+            .withSampler(sampler) //10 Traces por segundo
             //.withSampler(new SamplerConfiguration().withType(RateLimitingSampler.TYPE)
             //.withParam(10)) //10 Traces por segundo
-            .withReporter(new ReporterConfiguration().withLogSpans(true))
+            .withReporter(reporter)
             .getTracer();
     }
 }
